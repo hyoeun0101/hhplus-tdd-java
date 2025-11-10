4307367e2a549460a04431953bc301e8ae7b34b5
khe <kimhyoeun0101@gmail.com>
2025-11-10 16:21:23 +0900

    feat: 포인트 내역 조회 기능 단위테스트 작성 및 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,13 +1,18 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
     private final UserPointTable userPointTable;
+
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint getUserPoint(long userId) {
 
@@ -17,5 +22,9 @@ public class PointService {
             throw new IllegalArgumentException("userId [" + userId + "] not found");
         }
         return userPoint;
+    }
+
+    public List<PointHistory> getPointHistories(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -22,6 +24,9 @@ class PointServiceTest {
 
     @Mock
     UserPointTable userPointTable;
+
+    @Mock
+    PointHistoryTable pointHistoryTable;
 
     @Test
     @DisplayName("존재하는 유저ID로 포인트 조회 성공")
@@ -50,6 +55,39 @@ class PointServiceTest {
         //when & then
         assertThrows(IllegalArgumentException.class, () -> pointService.getUserPoint(invalidUserId));
         verify(userPointTable, times(1)).selectById(invalidUserId);
+    }
+
+
+    @Test
+    @DisplayName("유저ID로 포인트 내역 조회 성공")
+    public void givenUserId_whenGetPointHistories_thenReturnPointHistoryList() {
+        //given
+        long validUserId = 1L;
+        List<PointHistory> expected = List.of(
+                new PointHistory(1L, validUserId, 1000L, TransactionType.CHARGE, Instant.parse("2025-11-10T00:00:00Z").toEpochMilli())
+                , new PointHistory(1L, validUserId, 500L, TransactionType.USE, Instant.parse("2025-11-10T00:00:00Z").toEpochMilli())
+        );
+        when(pointHistoryTable.selectAllByUserId(validUserId)).thenReturn(expected);
+
+        //when
+        List<PointHistory> pointHistories = pointService.getPointHistories(validUserId);
+
+        //then
+        assertThat(pointHistories).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("유저 ID로 포인트 내역 조회 시 내역이 없으면 빈 리스트 반환")
+    public void givenUserId_whenGetPointHistories_thenReturnEmptyList() {
+        //given
+        long validUserId = 1L;
+        when(pointHistoryTable.selectAllByUserId(validUserId)).thenReturn(List.of());
+
+        //when
+        List<PointHistory> pointHistories = pointService.getPointHistories(validUserId);
+
+        //then
+        assertThat(pointHistories).isEqualTo(List.of());
     }
 
 }
